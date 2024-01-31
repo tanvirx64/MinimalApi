7ad52ca2a22c11d649a5f8c72a7624fee5a8c11a
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-02-01 00:22:09 +0600

    Coupon Create, Read endpoints added

---
+++ CouponApi/Data/CouponStore.cs
@@ -0,0 +1,12 @@
+﻿using CouponApi.Models;
+
+namespace CouponApi.Data
+{
+    public static class CouponStore
+    {
+        public static List<Coupon> couponList = new List<Coupon>() {
+            new Coupon() {Id = 1, Name = "100FF", Percent = 10, IsActive = true, Created = DateTime.Now },
+            new Coupon() {Id = 2, Name = "200FF", Percent = 20, IsActive = false, Created = DateTime.Now },
+        };
+    }
+}
--- CouponApi/Program.cs
+++ CouponApi/Program.cs
@@ -1,3 +1,7 @@
+using CouponApi.Data;
+using CouponApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,13 +18,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/hello/{id:int}",(int id) => {
-    return Results.Ok("Id = " + id);
-});
-app.MapPost("/hello_post", () => {
-    return Results.Ok("Hello World!");
-});
+app.MapGet("/api/coupons",() => {
+    return Results.Ok(CouponStore.couponList);
+}).WithName("Coupons");
 
+app.MapGet("/api/coupon/{id:int}", (int id) => {
+    return Results.Ok(CouponStore.couponList.FirstOrDefault(s=>s.Id == id));
+}).WithName("GetCoupon");
+
+app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
+    if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+    {
+        return Results.BadRequest("Invalid Coupon Id or Name!");
+    }
+    if (CouponStore.couponList.FirstOrDefault(x => x.Name.ToLower() == coupon.Name.ToLower()) != null)
+    {
+        return Results.BadRequest("Coupon Name Already Exist!");
+    }
+    coupon.Id = CouponStore.couponList.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
+    coupon.Created = DateTime.Now;
+    CouponStore.couponList.Add(coupon);
+
+    //return Results.Created($"/api/coupon/{coupon.Id}",coupon);
+    return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, coupon);
+}).WithName("CreateCoupon");
+
+app.MapPut("api/coupon", () => { });
+
+app.MapDelete("api/coupon/{id:int}", (int id) => { });
 
 app.UseHttpsRedirection();
 
