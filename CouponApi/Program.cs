05a2aba9b876bf7d91aa17126323aba32bc7ca2d
Tanvir Ahmed <tanvircsdev@gmail.com>
2024-01-31 22:28:16 +0600

    Test Endpoint added

--- CouponApi/Program.cs
+++ CouponApi/Program.cs
@@ -14,31 +14,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("/hello/{id:int}",(int id) => {
+    return Results.Ok("Id = " + id);
+});
+app.MapPost("/hello_post", () => {
+    return Results.Ok("Hello World!");
+});
+
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
